21abd9b1f1a4f05190a01b681286cf5b27bb56e0
Boris Krit <scriptboris@gmail.com>
2019-11-13 20:01:34 +0800

    c r: Add initializator

---
+++ DataGridSam/DataGridInit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataGridSam
+{
+    [Xamarin.Forms.Internals.Preserve(AllMembers = true)]
+    public static class DataGridSamInit
+    {
+        // For initialization in PCL part Xamarin.Forms
+        public static void Init()
+        {
+        }
+    }
+}
